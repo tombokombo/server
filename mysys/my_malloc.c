3f126e44f535bebf0518397b9f05a72d53697913
Daniel Black <daniel@mariadb.org>
2021-02-17 16:09:47 +1100

    create my_{malloc,calloc,free}_aligned functions
    Created based of pfs implementation

--- mysys/my_malloc.c
+++ mysys/my_malloc.c
@@ -19,6 +19,11 @@
 #include "mysys_err.h"
 #include <m_string.h>
 
+#if !defined(HAVE_POSIX_MEMALIGN) && defined(HAVE_MEMALIGN)
+#include <malloc.h>
+#endif
+
+
 struct my_memory_header
 {
   PSI_thread *m_owner;
@@ -249,3 +254,85 @@ char *my_strndup(PSI_memory_key key, const char *from, size_t length, myf my_fla
   DBUG_RETURN(ptr);
 }
 
+/**
+  Allocate an aligned block of memory.
+
+  @param size      The size of the memory block in bytes.
+  @param alignment The alignment of the memory block in bytes.
+
+  @return A pointer to the allocated memory block, or NULL on failure.
+*/
+void *my_malloc_aligned(size_t size, size_t alignment)
+{
+  void *ptr;
+  DBUG_ENTER("my_malloc_aligned");
+  DBUG_PRINT("my",("size: %zu align: %zu", size, alignment));
+
+#ifdef HAVE_POSIX_MEMALIGN
+  /* Linux + BSDs */
+  if (posix_memalign(& ptr, alignment, size))
+    DBUG_RETURN(NULL);
+#elif defined(HAVE_MEMALIGN)
+  /* Solaris */
+  ptr= memalign(alignment, size);
+#elif defined(HAVE_ALIGNED_MALLOC)
+  /* Windows */
+  ptr= _aligned_malloc(size, alignment);
+#else
+  ptr= malloc(size);
+#endif
+  DBUG_ASSERT(alignment && !(alignment & (alignment - 1)));
+  DBUG_ASSERT(((size_t)ptr) % alignment == 0);
+
+  DBUG_RETURN(ptr);
+}
+
+/**
+  Allocate an aligned block of zero initialized memory.
+
+  @param size      The size of the memory block in bytes.
+  @param alignment The alignment of the memory block in bytes.
+
+  @return A pointer to the allocated memory block, or NULL on failure.
+*/
+void *my_calloc_aligned(size_t size, size_t alignment)
+{
+  void *ptr;
+  DBUG_ENTER("my_calloc_aligned");
+  DBUG_PRINT("my",("size: %zu align: %zu", size, alignment));
+  ptr= my_malloc_aligned(size, alignment);
+  if (ptr)
+  {
+    memset(ptr, 0, size);
+  }
+
+  DBUG_RETURN(ptr);
+}
+
+/**
+  Free memory allocated with my_malloc_aligned
+
+  @param ptr Pointer to the memory allocated by my_malloc_aligned.
+*/
+void my_free_aligned(void *ptr)
+{
+  DBUG_ENTER("my_free_aligned");
+  DBUG_PRINT("my",("ptr: %p", ptr));
+  if (ptr == NULL)
+    DBUG_VOID_RETURN;
+
+#ifdef HAVE_POSIX_MEMALIGN
+  /* Allocated with posix_memalign() */
+  free(ptr);
+#elif defined(HAVE_MEMALIGN)
+  /* Allocated with memalign() */
+  free(ptr);
+#elif defined(HAVE_ALIGNED_MALLOC)
+  /* Allocated with _aligned_malloc() */
+  _aligned_free(ptr);
+#else
+  /* Allocated with malloc() */
+  free(ptr);
+#endif
+  DBUG_VOID_RETURN;
+}
