24e02fd94e33c93e81b491b82501f5cd08fdcc9b
Thirunarayanan Balathandayuthapani <thiru@mariadb.com>
2021-03-10 19:27:03 +0530

    MDEV-24626 Remove synchronous write of page0 and flushing file during file creation
    InnoDB holds dict_sys mutex while doing file i/o operation
    during creation of table. InnoDB remove the page0 write and
    flushing the file then InnoDB should handle this scenario
    during recovery. InnoDB may encounter zero filled page0 while
    processing FILE redo log records. In that case, InnoDB should
    defer the tablespace initialization. After parsing all redo log
    records, InnoDB should initialize the tablespace based on
    redo log of page0.

--- storage/innobase/fil/fil0fil.cc
+++ storage/innobase/fil/fil0fil.cc
@@ -360,8 +360,9 @@ fil_node_t* fil_space_t::add(const char* name, pfs_os_file_t handle,
 
 /** Open a tablespace file.
 @param node  data file
+@param validate validate the page0
 @return whether the file was successfully opened */
-static bool fil_node_open_file_low(fil_node_t *node)
+static bool fil_node_open_file_low(fil_node_t *node, bool validate=true)
 {
   ut_ad(!node->is_open());
   ut_ad(node->space->is_closing());
@@ -396,7 +397,7 @@ static bool fil_node_open_file_low(fil_node_t *node)
   }
 
   if (node->size);
-  else if (!node->read_page0() || !fil_comp_algo_validate(node->space))
+  else if (!node->read_page0(validate) || !fil_comp_algo_validate(node->space))
   {
     os_file_close(node->handle);
     node->handle= OS_FILE_CLOSED;
@@ -419,8 +420,9 @@ static bool fil_node_open_file_low(fil_node_t *node)
 
 /** Open a tablespace file.
 @param node  data file
+@param validate_page0 validate the page0
 @return whether the file was successfully opened */
-static bool fil_node_open_file(fil_node_t *node)
+bool fil_node_open_file(fil_node_t *node, bool validate_page0)
 {
   mysql_mutex_assert_owner(&fil_system.mutex);
   ut_ad(!node->is_open());
@@ -452,7 +454,7 @@ static bool fil_node_open_file(fil_node_t *node)
     }
   }
 
-  return fil_node_open_file_low(node);
+  return fil_node_open_file_low(node, validate_page0);
 }
 
 /** Close the file handle. */
@@ -651,7 +653,7 @@ fil_space_extend_must_retry(
 }
 
 /** @return whether the file is usable for io() */
-ATTRIBUTE_COLD bool fil_space_t::prepare(bool have_mutex)
+ATTRIBUTE_COLD bool fil_space_t::prepare(bool have_mutex, bool validate_page0)
 {
   ut_ad(referenced());
   if (!have_mutex)
@@ -661,7 +663,8 @@ ATTRIBUTE_COLD bool fil_space_t::prepare(bool have_mutex)
   ut_ad(!id || purpose == FIL_TYPE_TEMPORARY ||
         node == UT_LIST_GET_FIRST(chain));
 
-  const bool is_open= node && (node->is_open() || fil_node_open_file(node));
+  const bool is_open= node && (node->is_open()
+                               || fil_node_open_file(node, validate_page0));
 
   if (!is_open)
     release();
@@ -1432,7 +1435,7 @@ fil_write_flushed_lsn(
 @param id      tablespace identifier
 @return tablespace
 @retval nullptr if the tablespace is missing or inaccessible */
-fil_space_t *fil_space_t::get(ulint id)
+fil_space_t *fil_space_t::get(ulint id, bool validate_page0)
 {
   mysql_mutex_lock(&fil_system.mutex);
   fil_space_t *space= fil_space_get_by_id(id);
@@ -1442,8 +1445,9 @@ fil_space_t *fil_space_t::get(ulint id)
   if (n & STOPPING)
     space= nullptr;
 
-  if ((n & CLOSING) && !space->prepare())
+  if ((n & CLOSING) && !space->prepare(false, validate_page0)) {
     space= nullptr;
+  }
 
   return space;
 }
@@ -2382,44 +2386,7 @@ err_exit:
 		crypt_data->fill_page0(flags, page);
 	}
 
-	if (ulint zip_size = fil_space_t::zip_size(flags)) {
-		page_zip_des_t	page_zip;
-		page_zip_set_size(&page_zip, zip_size);
-		page_zip.data = page + srv_page_size;
-#ifdef UNIV_DEBUG
-		page_zip.m_start = 0;
-#endif /* UNIV_DEBUG */
-		page_zip.m_end = 0;
-		page_zip.m_nonempty = 0;
-		page_zip.n_blobs = 0;
-
-		buf_flush_init_for_writing(NULL, page, &page_zip, false);
-
-		*err = os_file_write(IORequestWrite, path, file,
-				     page_zip.data, 0, zip_size);
-	} else {
-		buf_flush_init_for_writing(NULL, page, NULL,
-					   fil_space_t::full_crc32(flags));
-
-		*err = os_file_write(IORequestWrite, path, file,
-				     page, 0, srv_page_size);
-	}
-
 	aligned_free(page);
-
-	if (*err != DB_SUCCESS) {
-		ib::error()
-			<< "Could not write the first page to"
-			<< " tablespace '" << path << "'";
-		goto err_exit;
-	}
-
-	if (!os_file_flush(file)) {
-		ib::error() << "File flush of tablespace '"
-			<< path << "' failed";
-		*err = DB_ERROR;
-		goto err_exit;
-	}
 
 	if (has_data_dir) {
 		/* Make the ISL file if the IBD file is not
@@ -2973,7 +2940,7 @@ fil_ibd_load(
 
 	/* Read and validate the first page of the tablespace.
 	Assign a tablespace name based on the tablespace type. */
-	switch (file.validate_for_recovery()) {
+	switch (dberr_t err= file.validate_for_recovery()) {
 		os_offset_t	minimum_size;
 	case DB_SUCCESS:
 		if (file.space_id() != space_id) {
@@ -2986,6 +2953,8 @@ fil_ibd_load(
 				<< space_id << ".";
 			return(FIL_LOAD_ID_CHANGED);
 		}
+		/* fall through */
+	case DB_DEFER_TABLESPACE:
 		/* Get and test the file size. */
 		size = os_file_get_size(file.handle());
 
@@ -3006,16 +2975,16 @@ fil_ibd_load(
 				<< file.filepath() << "' is only " << size
 				<< ", should be at least " << minimum_size
 				<< "!";
+		} else if (err == DB_DEFER_TABLESPACE) {
+			return FIL_LOAD_DEFER;
 		} else {
 			/* Everything is fine so far. */
 			break;
 		}
 
 		/* fall through */
-
 	case DB_TABLESPACE_EXISTS:
 		return(FIL_LOAD_INVALID);
-
 	default:
 		return(FIL_LOAD_NOT_FOUND);
 	}
